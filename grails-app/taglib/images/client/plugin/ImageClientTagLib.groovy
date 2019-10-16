f987295055e6aca002e629f4642646a2e53acd0d
pal155 <Doug.Palmer@csiro.au>
2019-10-16 12:00:56 +1100

    Use request.isUserInRole for checking to see whether a user can tag an image as preferred. Return an informative message if the lists tool couldn't match a name.

--- grails-app/taglib/images/client/plugin/ImageClientTagLib.groovy
+++ grails-app/taglib/images/client/plugin/ImageClientTagLib.groovy
@@ -1,27 +1,33 @@
 package images.client.plugin
 
 import au.org.ala.web.AuthService
+import grails.config.Config
+import grails.core.support.GrailsConfigurationAware
 
 /**
  * Image Client Tag Lib to be used in the gsp related to image client plugin feature
  *
  */
-class ImageClientTagLib {
+class ImageClientTagLib implements GrailsConfigurationAware {
 
     AuthService authService
     static namespace = 'imageClient'
 
-    /**
+    List<String> allowedRoles = []
+
+    @Override
+    void setConfiguration(Config config) {
+        def roleList = config.getProperty("allowedImageEditingRoles", "")
+        allowedRoles = roleList ? roleList.split(",") : []
+    }
+/**
      *
      *  Outputs true if user is logged in and user role is in the allowable configured roles: allowedImageEditingRoles (Note that the IP must also match the authorised system IP for the user).
      *  Otherwise, outputs false.
      *
      */
     def checkAllowableEditRole = { attrs ->
-        def allowedRolesConfig = grailsApplication.config.get("allowedImageEditingRoles")
-        List<String> allowedRoles = allowedRolesConfig ? allowedRolesConfig.split(","):[]
-        def currentUserRoles = authService?.getUserId() ? authService.getUserForUserId(authService.getUserId())?.roles : []
-        boolean match = currentUserRoles.any {allowedRoles.contains(it)}
+        boolean match = allowedRoles.any { request.isUserInRole(it) }
         out << match;
     }
 
